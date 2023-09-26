0fd4c819b2a7259005aeda9f569dce229830311a
PITPL-Akshay-Prajapati <akshay@pseudocode.com>
2023-09-26 14:54:08 +0530

    link search functionality changes

--- frappe/desk/search.py
+++ frappe/desk/search.py
@@ -33,7 +33,7 @@ def search_link(
 	txt,
 	query=None,
 	filters=None,
-	page_length=20,
+	# page_length=100,
 	searchfield=None,
 	reference_doctype=None,
 	ignore_user_permissions=False,
@@ -43,7 +43,7 @@ def search_link(
 		txt.strip(),
 		query,
 		searchfield=searchfield,
-		page_length=page_length,
+		# page_length=page_length,
 		filters=filters,
 		reference_doctype=reference_doctype,
 		ignore_user_permissions=ignore_user_permissions,
@@ -55,173 +55,84 @@ def search_link(
 # this is called by the search box
 @frappe.whitelist()
 def search_widget(
-	doctype,
-	txt,
-	query=None,
-	searchfield=None,
-	start=0,
-	page_length=20,
-	filters=None,
-	filter_fields=None,
-	as_dict=False,
-	reference_doctype=None,
-	ignore_user_permissions=False,
+    doctype,
+    txt,
+    query=None,
+    searchfield=None,
+    start=0,
+    # page_length=100,
+    filters=None,
+    filter_fields=None,
+    as_dict=False,
+    reference_doctype=None,
+    ignore_user_permissions=False,
 ):
 
-	start = cint(start)
+    start = cint(start)
 
-	if isinstance(filters, string_types):
-		filters = json.loads(filters)
+    if isinstance(filters, string_types):
+        filters = json.loads(filters)
 
-	if searchfield:
-		sanitize_searchfield(searchfield)
+    if searchfield:
+        sanitize_searchfield(searchfield)
 
-	if not searchfield:
-		searchfield = "name"
+    if not searchfield:
+        searchfield = "name"
+	
+	# # Remove results where Customer is the filter value
+	# if "Customer" in filters:
+	# 	customer_filter_value = filters["Customer"]
+	# 	intersection_results = [result for result in intersection_results if result != customer_filter_value]
 
-	standard_queries = frappe.get_hooks().standard_queries or {}
+    if not txt:  # If no search term, fetch all values
+        results = frappe.get_all(
+            doctype,
+            filters=filters,
+	    	page_length=20,
+            fields=["name"],
+            start=start,
+            # limit_page_length=page_length,
+            ignore_permissions=True,
+        )
+    else:
+        # Split search term into words
+        search_terms = txt.strip().split()
+        # Initialize an empty set for intersection
+        intersection_results = set()
 
-	if query and query.split()[0].lower() != "select":
-		# by method
-		try:
-			is_whitelisted(frappe.get_attr(query))
-			frappe.response["values"] = frappe.call(
-				query, doctype, txt, searchfield, start, page_length, filters, as_dict=as_dict
-			)
-		except frappe.exceptions.PermissionError as e:
-			if frappe.local.conf.developer_mode:
-				raise e
-			else:
-				frappe.respond_as_web_page(
-					title="Invalid Method", html="Method not found", indicator_color="red", http_status_code=404
-				)
-			return
-		except Exception as e:
-			raise e
-	elif not query and doctype in standard_queries:
-		# from standard queries
-		search_widget(
-			doctype, txt, standard_queries[doctype][0], searchfield, start, page_length, filters
-		)
-	else:
-		meta = frappe.get_meta(doctype)
+        for term in search_terms:
+            term_results = frappe.get_list(
+                doctype,
+                filters=filters,
+                fields=["name"],
+                or_filters=[
+                    [doctype, searchfield, "like", "%{0}%".format(term)]
+                ],
+                limit_start=start,
+                # limit_page_length=page_length,
+                ignore_permissions=True,
+            )
+        
+            term_results = {r.name for r in term_results}
+            # frappe.msgprint(str(term_results))
+            if not intersection_results:
+                intersection_results = term_results
+            else:
+                # Intersect with previous results
+                intersection_results &= term_results
 
-		if query:
-			frappe.throw(_("This query style is discontinued"))
-			# custom query
-			# frappe.response["values"] = frappe.db.sql(scrub_custom_query(query, searchfield, txt))
-		else:
-			if isinstance(filters, dict):
-				filters_items = filters.items()
-				filters = []
-				for f in filters_items:
-					if isinstance(f[1], (list, tuple)):
-						filters.append([doctype, f[0], f[1][0], f[1][1]])
-					else:
-						filters.append([doctype, f[0], "=", f[1]])
+        # Fetch full records based on intersection results
+        results = frappe.get_list(
+            doctype,
+	    	page_length=20,
+            filters={"name": ["in", list(intersection_results)]},
+	        limit_start=start,
+            # limit_page_length=page_length,
+            order_by="name",
+            ignore_permissions=True,
+        )
 
-			if filters == None:
-				filters = []
-			or_filters = []
-
-			translated_doctypes = frappe.cache().hget(
-				"translated_doctypes", "doctypes", get_translated_doctypes
-			)
-			# build from doctype
-			if txt:
-				field_types = [
-					"Data",
-					"Text",
-					"Small Text",
-					"Long Text",
-					"Link",
-					"Select",
-					"Read Only",
-					"Text Editor",
-				]
-				search_fields = ["name"]
-				if meta.title_field:
-					search_fields.append(meta.title_field)
-
-				if meta.search_fields:
-					search_fields.extend(meta.get_search_fields())
-
-				for f in search_fields:
-					fmeta = meta.get_field(f.strip())
-					if (doctype not in translated_doctypes) and (
-						f == "name" or (fmeta and fmeta.fieldtype in field_types)
-					):
-						or_filters.append([doctype, f.strip(), "like", "%{0}%".format(txt)])
-
-			if meta.get("fields", {"fieldname": "enabled", "fieldtype": "Check"}):
-				filters.append([doctype, "enabled", "=", 1])
-			if meta.get("fields", {"fieldname": "disabled", "fieldtype": "Check"}):
-				filters.append([doctype, "disabled", "!=", 1])
-
-			# format a list of fields combining search fields and filter fields
-			fields = get_std_fields_list(meta, searchfield or "name")
-			if filter_fields:
-				fields = list(set(fields + json.loads(filter_fields)))
-			formatted_fields = ["`tab%s`.`%s`" % (meta.name, f.strip()) for f in fields]
-
-			# find relevance as location of search term from the beginning of string `name`. used for sorting results.
-			formatted_fields.append(
-				"""locate({_txt}, `tab{doctype}`.`name`) as `_relevance`""".format(
-					_txt=frappe.db.escape((txt or "").replace("%", "").replace("@", "")), doctype=doctype
-				)
-			)
-
-			# In order_by, `idx` gets second priority, because it stores link count
-			from frappe.model.db_query import get_order_by
-
-			order_by_based_on_meta = get_order_by(doctype, meta)
-			# 2 is the index of _relevance column
-			order_by = "_relevance, {0}, `tab{1}`.idx desc".format(order_by_based_on_meta, doctype)
-
-			ptype = "select" if frappe.only_has_select_perm(doctype) else "read"
-			ignore_permissions = (
-				True
-				if doctype == "DocType"
-				else (cint(ignore_user_permissions) and has_permission(doctype, ptype=ptype))
-			)
-
-			if doctype in translated_doctypes:
-				page_length = None
-
-			values = frappe.get_list(
-				doctype,
-				filters=filters,
-				fields=formatted_fields,
-				or_filters=or_filters,
-				limit_start=start,
-				limit_page_length=page_length,
-				order_by=order_by,
-				ignore_permissions=ignore_permissions,
-				reference_doctype=reference_doctype,
-				as_list=not as_dict,
-				strict=False,
-			)
-
-			if doctype in translated_doctypes:
-				# Filtering the values array so that query is included in very element
-				values = (
-					v
-					for v in values
-					if re.search(f"{re.escape(txt)}.*", _(v.name if as_dict else v[0]), re.IGNORECASE)
-				)
-
-			# Sorting the values array so that relevant results always come first
-			# This will first bring elements on top in which query is a prefix of element
-			# Then it will bring the rest of the elements and sort them in lexicographical order
-			values = sorted(values, key=lambda x: relevance_sorter(x, txt, as_dict))
-
-			# remove _relevance from results
-			if as_dict:
-				for r in values:
-					r.pop("_relevance")
-				frappe.response["values"] = values
-			else:
-				frappe.response["values"] = [r[:-1] for r in values]
+    frappe.response["values"] = results
 
 
 def get_std_fields_list(meta, key):
@@ -240,14 +151,12 @@ def get_std_fields_list(meta, key):
 
 	return sflist
 
-
 def build_for_autosuggest(res):
-	results = []
-	for r in res:
-		out = {"value": r[0], "description": ", ".join(unique(cstr(d) for d in r if d)[1:])}
-		results.append(out)
-	return results
-
+    results = []
+    for r in res:
+        out = {"value": r["name"], "description": ""}
+        results.append(out)
+    return results
 
 def scrub_custom_query(query, key, txt):
 	if "%(key)s" in query:
